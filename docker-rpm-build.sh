7adbaf2736c201ffb880e15991fc9dd9fbbc25ba
peshay <peshay@users.noreply.github.com>
2018-05-18 10:28:14 +0200

    add possibility to use rpm macros
    Users could manage a rpmmacros file in their working directory in order to enhance their spec file.

--- docker-rpm-build.sh
+++ docker-rpm-build.sh
@@ -24,6 +24,7 @@ TOPDIR="${HOME}/rpmbuild"
 # copy sources and spec into rpmbuild's work dir
 cp "${VERBOSE:+-v}" -a --reflink=auto * "${TOPDIR}/SOURCES/"
 cp "${VERBOSE:+-v}" -a --reflink=auto "${SPEC}" "${TOPDIR}/SPECS/"
+test -f rpmmacros && cp "${VERBOSE:+-v}" -a --reflink=auto "rpmmacros" "${HOME}/.rpmmacros"
 SPEC="${TOPDIR}/SPECS/${SPEC##*/}"
 
 # build the RPMs
